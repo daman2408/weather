33bc54be6bba796d96ef5af38779e9e8d23f8390
Damanvir Mangat <daman@Damanvirs-MacBook-Pro.local>
2016-12-23 17:09:24 -0500

    added google geolocation api

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,57 +1,70 @@
-
-//get current weather conditions
+//google api call gets the most accurate gps coordinates, wrap the other api calls with this so that the coordinates can be used inside of them
 $.ajax({
-  url:"https://api.wunderground.com/api/a496a438d6e77ae4/conditions/q/autoip.json",
-  method: "GET",
+  url:"https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDbfFdoar93pQWmiW2rDPptDsIbUg4xGZU",
+  method: "POST",
   success: function(data) {
+    var longitude = data.location.lng;
+    var latitude = data.location.lat;
 
-    var cityName = data.current_observation.display_location.city;
-    var currentTemperature = data.current_observation.temp_c;
-    var feelsLike = data.current_observation.feelslike_c;
-    var weatherCondition = data.current_observation.weather;
+    //get current weather conditions
+    $.ajax({
+      url:"https://api.wunderground.com/api/a496a438d6e77ae4/conditions/q/autoip.json",
+      method: "GET",
+      success: function(data) {
 
-    $('#currentTemperature').text(Math.round(data.current_observation.temp_c) + '°C');
-    $('#feels-like').text('Feels like ' +  feelsLike + '°C');
-    $('#current-weather-condition').text(weatherCondition);
+        var cityName = data.current_observation.display_location.city;
+        var currentTemperature = data.current_observation.temp_c;
+        var feelsLike = data.current_observation.feelslike_c;
+        var weatherCondition = data.current_observation.weather;
 
-  }
-});
+        $('#currentTemperature').text(Math.round(data.current_observation.temp_c) + '°C');
+        $('#feels-like').text('Feels like ' +  feelsLike + '°C');
+        $('#current-weather-condition').text(weatherCondition);
 
-
-//get the daily forecast
-$.ajax({
-  url:"https://api.wunderground.com/api/a496a438d6e77ae4/forecast/q/autoip.json",
-  method: 'GET',
-  success: function(data) {
-    console.log(data);
-  }
-});
+      }
+    });
 
 
-//get the hourly forecast
-$.ajax({
-  url:"https://api.wunderground.com/api/a496a438d6e77ae4/hourly/q/autoip.json",
-  method: 'GET',
-  success: function(data) {
-    var hour1_temp = data.hourly_forecast[0].temp.metric + '°C';
-    var hour1_time = data.hourly_forecast[0].FCTTIME.civil;
-    var hour2_temp = data.hourly_forecast[1].temp.metric + '°C';
-    var hour3_temp = data.hourly_forecast[2].temp.metric + '°C';
+    //get the daily forecast
+    $.ajax({
+      url:"https://api.wunderground.com/api/a496a438d6e77ae4/forecast/q/autoip.json",
+      method: 'GET',
+      success: function(data) {
+        console.log(data);
+      }
+    });
 
-    $('#hour1').text(hour1_time);
 
-  }
-});
+    //get the hourly forecast
+    $.ajax({
+      url:"https://api.wunderground.com/api/a496a438d6e77ae4/hourly/q/autoip.json",
+      method: 'GET',
+      success: function(data) {
+        var hour1_temp = data.hourly_forecast[0].temp.metric + '°C';
+        var hour1_time = data.hourly_forecast[0].FCTTIME.civil;
+        var hour2_temp = data.hourly_forecast[1].temp.metric + '°C';
+        var hour3_temp = data.hourly_forecast[2].temp.metric + '°C';
 
-//get the location
-$.ajax({
-  url:"https://api.wunderground.com/api/a496a438d6e77ae4/geolookup/q/autoip.json",
-  method: 'GET',
-  success: function(data) {
-    var city = data.location.nearby_weather_stations.pws.station[0].city;
+        $('#hour1').text(hour1_time);
 
-    $('#city').text(city);
+      }
+    });
 
-    console.log(data);
+    //get the location
+    $.ajax({
+      url:"https://api.wunderground.com/api/a496a438d6e77ae4/geolookup/q/" + latitude + "," + longitude +  ".json",
+      method: 'GET',
+      success: function(data) {
+        var city = data.location.city;
+
+        $('#city').text(city);
+
+        console.log(data);
+      }
+    });
+
+  },
+  error: function(err) {
+    console.log(err);
   }
 });
