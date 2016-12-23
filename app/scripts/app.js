5796476c6986e1f52926bfe408b52d919b4fd0ec
Damanvir Mangat <daman@Damanvirs-MacBook-Pro.local>
2016-12-23 16:26:25 -0500

    changed to city

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -48,7 +48,7 @@ $.ajax({
   url:"https://api.wunderground.com/api/a496a438d6e77ae4/geolookup/q/autoip.json",
   method: 'GET',
   success: function(data) {
-    var city = data.location.nearby_weather_stations.pws.station[0].neighborhood;
+    var city = data.location.nearby_weather_stations.pws.station[0].city;
 
     $('#city').text(city);
 
