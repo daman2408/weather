d9e9c73485c2a1d02c58c4b4887052238c689394
Damanvir Mangat <daman@Damanvirs-MacBook-Pro.local>
2016-12-23 12:37:26 -0500

    weather is current

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,22 @@
+
+//get current weather conditions
+$.ajax({
+  url:"https://api.wunderground.com/api/a496a438d6e77ae4/conditions/q/autoip.json",
+  method: "GET",
+  success: function(data) {
+    console.log(data);
+
+    var currentTemperature = data.current_observation.temp_c;
+    var feelsLike = data.current_observation.feelslike_c;
+    var weatherCondition = data.current_observation.weather;
+    $('#currentTemperature').text(Math.round(data.current_observation.temp_c) + '°C');
+    $('#feels-like').text('Feels like ' +  feelsLike + '°C');
+    $('#current-weather-condition').text(weatherCondition)
+  }
+});
+
+
+//get the forecast
 $.ajax({
   url:"https://api.wunderground.com/api/a496a438d6e77ae4/forecast/q/autoip.json",
   method: 'GET',
@@ -6,11 +25,12 @@ $.ajax({
   }
 });
 
+//get local information
 $.ajax({
   url:"https://api.wunderground.com/api/a496a438d6e77ae4/geolookup/q/autoip.json",
   method: 'GET',
   success: function(data) {
     var city = data.location.nearby_weather_stations.pws.station[0].neighborhood;
-    console.log(city);
+    console.log(data);
   }
 });
