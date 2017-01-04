ecf53a7d73510b3b6dc00b2d17ab818a521ff823
Damanvir Mangat <daman@Damanvirs-MacBook-Pro.local>
2017-01-04 12:21:21 -0500

    5 day forecast

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,7 @@
 
   navigator.geolocation.getCurrentPosition(function(pos) {
+    var doc = document.body;
+    var celsius = '°C';
     var lat = pos.coords.latitude;
     var lng = pos.coords.longitude;
     /* MAKING CALLS TO WEATHER UNDERGROUND API */
@@ -14,22 +16,27 @@
             var feelsLike = data.current_observation.feelslike_c;
             var weatherCondition = data.current_observation.weather;
 
-            $('#currentTemperature').text(Math.round(data.current_observation.temp_c) + '°C');
-            $('#feels-like').text('Feels like ' +  feelsLike + '°C');
+            $('#currentTemperature').text(Math.round(data.current_observation.temp_c) + celsius);
+            $('#feels-like').text('Feels like ' +  feelsLike + celsius);
             $('#current-weather-condition').text(weatherCondition);
 
           }
         });
 
-
-        //get the daily forecast. this will be used to generate the 36-hour forecast
-        $.ajax({
-          url:"https://api.wunderground.com/api/a496a438d6e77ae4/forecast/q/" + lat + "," + lng +  ".json",
-          method: 'GET',
-          success: function(data) {
-            console.log(data);
-          }
-        });
+        // $.ajax({
+        //   url:"https://api.wunderground.com/api/a496a438d6e77ae4/forecast/q/" + lat + "," + lng +  ".json",
+        //   method: 'GET',
+        //   success: function(data) {
+        //
+        //     for (var i = 0; i < 3; i++) {
+        //       var day_temp = data.
+        //     }
+        //
+        //
+        //
+        //     console.log(data);
+        //   }
+        // });
 
         //get the hourly forecast
         $.ajax({
@@ -37,15 +44,12 @@
           method: 'GET',
           success: function(data) {
 
-            console.log(data);
-
             //display hourly forecast
             for (var i = 0; i < 3; i++) {
-              var doc = document.body;
-              var hour_temp = data.hourly_forecast[i].temp.metric + '°C';
+              var hour_temp = data.hourly_forecast[i].temp.metric + celsius;
               var hour_time = data.hourly_forecast[i].FCTTIME.civil;
               var hour_conditions = data.hourly_forecast[i].condition;
-              var feelsLike = 'Feels like ' + data.hourly_forecast[i].feelslike.metric + '°C';
+              var feelsLike = 'Feels like ' + data.hourly_forecast[i].feelslike.metric + celsius;
 
               // set the weather icon
               var hourly_weather_icon = data.hourly_forecast[i].icon_url;
@@ -83,18 +87,44 @@
               doc.getElementsByClassName('thirtySixTimeOfDay')[i].innerHTML = thirtySixHours[i].FCTTIME.civil;
 
               // set the temperature
-              doc.getElementsByClassName('thirtySixTemperature')[i].innerHTML = thirtySixHours[i].temp.metric + '°C';
+              doc.getElementsByClassName('thirtySixTemperature')[i].innerHTML = thirtySixHours[i].temp.metric + celsius;
 
               // set the weather condition
               doc.getElementsByClassName('thirtySix-weather-condition')[i].innerHTML = thirtySixHours[i].condition;
 
               // set the 'feels like' text
-              doc.getElementsByClassName('thirtySix-feels-like')[i].innerHTML = 'Feels like ' + thirtySixHours[i].feelslike.metric + '°C';
+              doc.getElementsByClassName('thirtySix-feels-like')[i].innerHTML = 'Feels like ' + thirtySixHours[i].feelslike.metric + celsius;
 
             }
+          }
+        });
 
-            console.log(thirtySixHours);
+        //get the daily forecast. this will be used to generate the 14 Day forecast
+        $.ajax({
+          url:"https://api.wunderground.com/api/a496a438d6e77ae4/forecast10day/q/" + lat + "," + lng +  ".json",
+          method: 'GET',
+          success: function(data) {
+            for (var i = 0; i < 5; i++) {
+              var dayOfWeek = data.forecast.simpleforecast.forecastday[i].date.weekday;
+              var day_weather_icon = (data.forecast.simpleforecast.forecastday[i].icon_url).replace(/http/, 'https');
+              var day_temp_high = data.forecast.simpleforecast.forecastday[i].high.celsius + celsius;
+              var day_temp_low = data.forecast.simpleforecast.forecastday[i].low.celsius + celsius;
+              var day_weather_condition = data.forecast.simpleforecast.forecastday[i].conditions;
 
+              // set the day of the week title-div
+              doc.getElementsByClassName('day')[i].innerHTML = dayOfWeek;
+
+              // set the weather icon for the day
+              doc.getElementsByClassName('daily-weather-icon')[i].src = day_weather_icon;
+
+              // inject daily high and low
+              doc.getElementsByClassName('daily_temp_high')[i].innerHTML = '<small class="text-muted">High: </small>' + day_temp_high;
+              doc.getElementsByClassName('daily_temp_low')[i].innerHTML = '<small class="text-muted">Low: </small>' + day_temp_low;
+
+              // inject weather condition for the day
+              doc.getElementsByClassName('daily-weather-condition')[i].innerHTML = day_weather_condition;
+
+            }
           }
         });
 
@@ -105,8 +135,6 @@
           success: function(data) {
             var city = data.location.city;
             $('#city').text(city);
-
-            console.log(data);
           }
         });
 
